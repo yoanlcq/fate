df8a103009238bcae94c5e444a29bd2abebcac7b
Yoan <yoanlecoq.io@gmail.com>
2017-07-24 14:21:00 +0200

    Update lib.rs

--- vek/src/lib.rs
+++ vek/src/lib.rs
@@ -4,6 +4,7 @@
 //!
 //! DO NOT USE (yet). This is very much a work-in progress, breaking changes happen all the time on a whim.
 
+// TODO have a story for using on non-Nightly channels
 // TODO: Implement correctness check for FrustumPlanes (i.e left <= right, etc)
 // TODO index vectors by range
 // TODO angle between two vectors
