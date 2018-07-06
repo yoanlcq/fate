3697c3d459f187e72fd909c35c7577ebbc27a30f
Yoan Lecoq <yoanlecoq.io@gmail.com>
2018-07-06 13:57:26 +0200

    Prepare for lighting; breaks build

--- game/src/gamegl.rs
+++ game/src/gamegl.rs
@@ -26,33 +26,69 @@ pub fn gl_debug_message_callback(msg: &gx::DebugMessage) {
     };
 }
 
-const ATTRIB_POSITION_VEC3F32: GLuint = 0;
-const ATTRIB_COLOR_RGBAF32: GLuint = 1;
+#[repr(u32)]
+pub enum VAttrib {
+    PositionVec4f32 = 0,
+    NormalVec4f32 = 1,
+    TangentVec4f32 = 2,
+    BiTangentVec4f32 = 3,
+    ColorRgbau8 = 4,
+}
 
 static VS_SRC: &'static [u8] = b"
 #version 450 core
 
-uniform mat4 u_mvp;
+uniform mat4 u_viewproj_matrix;
+uniform mat4 u_model_matrix;
+uniform mat4 u_normal_matrix;
 
-layout(location = 0) in vec3 a_position;
-layout(location = 1) in vec4 a_color;
+layout(location = 0) in vec4 a_position;
+layout(location = 1) in vec4 a_normal;
+layout(location = 4) in vec4 a_color;
 
+out vec4 v_position;
+out vec4 v_normal;
 out vec4 v_color;
 
 void main() {
+    v_position = u_model_matrix * vec4(a_position.xyz, 1.0);
+    v_normal = u_normal_matrix * vec4(a_normal.xyz, 1.0);
     v_color = a_color;
-    gl_Position = u_mvp * vec4(a_position, 1.0);
+    gl_Position = u_viewproj_matrix * v_position;
 }
 ";
 static FS_SRC: &'static [u8] = b"
 #version 450 core
 
+uniform vec3 u_view_position; 
+uniform vec3 u_light_position; 
+uniform vec3 u_light_color;
+
+in vec4 v_position;
+in vec4 v_normal;
 in vec4 v_color;
 
 out vec4 f_color;
 
 void main() {
-    f_color = v_color;
+    // ambient
+    float ambient_strength = 0.1;
+    vec3 ambient = ambient_strength * u_light_color;
+  	
+    // diffuse 
+    vec3 norm = normalize(v_normal);
+    vec3 light_dir = normalize(u_light_position - v_position);
+    float diff = max(dot(norm, light_dir), 0.0);
+    vec3 diffuse = diff * u_light_color;
+    
+    // specular
+    float specular_strength = 0.5;
+    vec3 view_dir = normalize(u_view_position - v_position);
+    vec3 reflect_dir = reflect(-light_dir, norm);
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), 32);
+    vec3 specular = specular_strength * spec * u_light_color;
+
+    f_color = v_color * vec4(ambient + diffuse + specular, 1.0);
 }
 ";
 
@@ -234,6 +270,14 @@ impl GLSystem {
             let model = Mat4::from(xform);
             let mvp = proj * view * model;
             self.prog.set_u_mvp(&mvp);
+
+            // TODO: set these uniforms
+            // uniform mat4 u_viewproj_matrix;
+            // uniform mat4 u_model_matrix;
+            // uniform mat4 u_normal_matrix;
+            // uniform vec3 u_view_position; 
+            // uniform vec3 u_light_position; 
+            // uniform vec3 u_light_color;
 
             /*
             unsafe {
